0bd6b87be55fcb2981111450dce7d4686b380ef3
no3 <nfauconn@student.42.fr>
2023-06-14 14:24:54 +0200

    nginx ok!!!!!!!!!!!!!

--- t/init_domain.sh
+++ t/init_domain.sh
@@ -6,26 +6,26 @@ i=0
 
 while [ $i -le 10 ]
 do
-    if grep -q "web$i.tliot.42.fr" /etc/hosts; then
-        echo "web$i.tliot.42.fr exist !"
+    if grep -q "web$i.nfauconn.42.fr" /etc/hosts; then
+        echo "web$i.nfauconn.42.fr exist !"
     else
-        echo "web$i.tliot.42.fr create "
-        echo "$ip web$i.tliot.42.fr" >> /etc/hosts
+        echo "web$i.nfauconn.42.fr create "
+        echo "$ip web$i.nfauconn.42.fr" >> /etc/hosts
     fi
   ((i++))
 done
 
 
-if grep -q "adminer.tliot.42.fr" /etc/hosts; then
-    echo "adminer.tliot.42.fr exist !"
+if grep -q "adminer.nfauconn.42.fr" /etc/hosts; then
+    echo "adminer.nfauconn.42.fr exist !"
 else
-    echo "adminer.tliot.42.fr create "
-    echo "$ip adminer.tliot.42.fr" >> /etc/hosts
+    echo "adminer.nfauconn.42.fr create "
+    echo "$ip adminer.nfauconn.42.fr" >> /etc/hosts
 fi
 
-if grep -q -E "^tliot.42.fr$" /etc/hosts; then
-    echo "tliot.42.fr exist !"
+if grep -q -E "^nfauconn.42.fr$" /etc/hosts; then
+    echo "nfauconn.42.fr exist !"
 else
-    echo "tliot.42.fr create "
-    echo "$ip tliot.42.fr" >> /etc/hosts
+    echo "nfauconn.42.fr create "
+    echo "$ip nfauconn.42.fr" >> /etc/hosts
 fi
