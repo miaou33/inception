a7af3eed069638024725160150d57105ee439417
no3 <meow@no3arch>
2023-08-26 17:29:10 +0200

    DEL: .env

---
+++ setupvm.sh
@@ -0,0 +1,9 @@
+sudo pacman -Syu
+
+sudo pacman -S virtualbox-guest-utils
+#si pas deja installe : l'installer puis reboot
+
+xrandr --output Virtual1 --mode 2560x1600
+# Virtual1 ou autre nom cf output of xrandr
+
+
