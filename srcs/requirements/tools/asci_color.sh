62d80b1e61f4e6f8cbd38cb9babc1d9802bd8340
no3 <nfauconn@student.42.fr>
2023-08-30 16:04:05 +0200

    commit pause

---
+++ srcs/requirements/tools/asci_color.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+comment=$'\033[0;30m'
+red=$'\033[0;31m'
+green=$'\033[0;32m'
+orange=$'\033[0;33m'
+blue=$'\033[34m'
+purple=$'\033[1;35m'
+boldlightgreen=$'\033[1;36m'
+boldwhite=$'\033[1;37m'
+#eight=$'\033[1;38m' #same as boldbeige
+boldlightgrey=$'\033[1;39m'
+reset=$'\033[0m'
+
