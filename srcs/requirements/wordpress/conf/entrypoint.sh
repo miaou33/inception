8997bfd0cc9ba06090233365c510db2fb6022d28
no3 <nfauconn@student.42.fr>
2023-09-04 19:26:38 +0200

    var for domain name

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -19,28 +19,26 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
     # Creation of wp-config.php
     wp config create                        \
     --dbname=$DB_NAME                       \
-    --dbuser=$DB_USR                       \
-    --dbpass=$DB_USR_PWD                \
+    --dbuser=$DB_USR						\
+    --dbpass=$DB_USR_PWD					\
     --dbhost=mariadb
             
     # Installation of WordPress
     wp core install                         \
-    --url=https://nfauconn.42.fr            \
-    --title="[ WordPress by nfauconn ]"      \
-    --admin_user=$WP_ROOT_USR              \
-    --admin_password=$WP_ROOT_PWD       \
-    --admin_email=$WP_ROOT_MAIL            \
+    --url=$DOMAIN_NAME						\
+    --title="Inception"						\
+    --admin_user=$WP_ROOT_USR               \
+    --admin_password=$WP_ROOT_PWD			\
+    --admin_email=$WP_ROOT_MAIL				\
     --skip-email
 
     # Creation of a new user
     wp user create                          \
-    $WP_USR_NAME                           \
+    $WP_USR_NAME							\
     $WP_USR_MAIL                            \
     --user_pass=$WP_USR_PWD
 fi
 
 # Start PHP and Nginx
 exec "$@"
-#exec "php-fpm81 --nodaemonize --fpm-config /etc/php81/php-fpm.d/www.conf -y /etc/php81/php-fpm.conf --force-stderr"
-#exec "php-fpm81 -F -R >> /var/log/php81/fpm-start.log 2>&1"
 
